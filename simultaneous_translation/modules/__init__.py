908d57a6603555bb2f4a610ba105ec3974d5f48b
Aditi Jain <67906321+AditiJain14@users.noreply.github.com>
2023-02-02 16:50:16 -0800

    Create __init__.py

---
+++ simultaneous_translation/modules/__init__.py
@@ -0,0 +1,35 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import os
+import importlib
+# from fairseq import registry
+
+# (
+#     build_monotonic_attention,
+#     register_monotonic_attention,
+#     MONOTONIC_ATTENTION_REGISTRY,
+#     _,
+# ) = registry.setup_registry("--simul-type")
+
+# for file in sorted(os.listdir(os.path.dirname(__file__))):
+#     if file.endswith(".py") and not file.startswith("_"):
+#         model_name = file[: file.find(".py")]
+#         importlib.import_module(
+#             "examples.simultaneous_translation.modules." + model_name
+#         )
+
+from fairseq import registry
+(
+    build_monotonic_attention,
+    register_monotonic_attention,
+    MONOTONIC_ATTENTION_REGISTRY,
+    _,
+) = registry.setup_registry('--simul-type')
+
+from .monotonic_multihead_attention import MonotonicAttention, ChunkwiseAttention, MonotonicInfiniteLookbackAttention, WaitKAttention
+from .monotonic_transformer_layer import TransformerMonotonicEncoderLayer, TransformerMonotonicDecoderLayer
+
